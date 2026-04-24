726b482683f59fefcab42cba6c19a67ac3aa625a
sebastian <leybrak22@gmail.com>
2026-04-24 00:46:19 -0500

    Fix: asgi

--- core/asgi.py
+++ core/asgi.py
@@ -1,11 +1,13 @@
 # core/asgi.py
 import os
+
+# ✅ DEBE ir ANTES de cualquier import de Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from negocios.middleware import JWTWebSocketMiddleware
 import negocios.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
