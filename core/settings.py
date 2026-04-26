b3d503d7461a004bde55a760e20a52f05fe89e4c
sebastian <leybrak22@gmail.com>
2026-04-26 01:36:00 -0500

    fix jwt

--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'django.contrib.sessions',
     'django.contrib.messages',
